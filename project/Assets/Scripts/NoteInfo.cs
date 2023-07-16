b23f133221d8d7adb4f11be6a197fb3b3014241f
insiderhj <insiderhj@gmail.com>
2023-07-17 06:53:05 +0900

    json reader

--- project/Assets/Scripts/NoteInfo.cs
+++ project/Assets/Scripts/NoteInfo.cs
@@ -2,36 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoteInfo : MonoBehaviour
+[System.Serializable]
+public class NoteInfo
 {
-	private float time;
-	private string image;
-	private List<Vector3> jointList;
-	private List<string> answerBones;
-
-	public NoteInfo() {
-	}
-
-	public void Initiate(float _time, string _image, List<Vector3> _jointList, List<string> _answerBones) {
-		time = _time;
-		image = _image;
-		jointList = _jointList;
-		answerBones = _answerBones;
-	}
-
-	public List<Vector3> GetJoints() {
-		return jointList;
-	}
-
-	public string GetImagePath() {
-		return image;
-	}
-
-	public float GetTime() {
-		return time;
-	}
-
-	public List<string> GetCheckBones() {
-		return answerBones;
-	}
+	public int time;
+	public string image;
+	public List<Joint> joints;
+	public List<string> answerBones;
 }
---
+++ project/Assets/Scripts/NoteObjectProp.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObjectProp : MonoBehaviour
+{
+	public int time;
+	public string image;
+	public List<Vector3> joints;
+	public List<string> answerBones;
+
+	public void Initiate(NoteInfo noteInfo) {
+		time = noteInfo.time;
+		image = noteInfo.image;
+		joints = new List<Vector3>();
+		foreach (Joint joint in noteInfo.joints)
+			joints.Add(joint.GetVector());
+		answerBones = noteInfo.answerBones;
+	}
+}
