9abf21db3f0e0a1d66d7376b70bf4b36d688b3c8
insiderhj <insiderhj@gmail.com>
2023-07-17 05:30:37 +0900

    change gradation

--- project/Assets/Scripts/NoteInfo.cs
+++ project/Assets/Scripts/NoteInfo.cs
@@ -2,24 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoteInfo
+public class NoteInfo : MonoBehaviour
 {
-	private float appearTime;
-	private string imagePath;
-	private List<Vector3> vectorList;
+	private float time;
+	private string image;
+	private List<Vector3> jointList;
+	private List<string> answerBones;
 
 	public NoteInfo() {
 	}
 
-	public List<Vector3> GetVectors() {
-		return vectorList;
+	public void Initiate(float _time, string _image, List<Vector3> _jointList, List<string> _answerBones) {
+		time = _time;
+		image = _image;
+		jointList = _jointList;
+		answerBones = _answerBones;
+	}
+
+	public List<Vector3> GetJoints() {
+		return jointList;
 	}
 
 	public string GetImagePath() {
-		return imagePath;
+		return image;
 	}
 
-	public float GetAppearTime() {
-		return appearTime;
+	public float GetTime() {
+		return time;
+	}
+
+	public List<string> GetCheckBones() {
+		return answerBones;
 	}
 }
